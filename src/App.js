e8e99829760fa9b3026f18c6ba538e7bd2088d16
eman hamid <emanhharki@gmail.com>
2021-05-22 09:36:56 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,16 +1,1 @@
-import React, { Component } from 'react';
-
-class App extends Component {
-	render() {
-		// your code in the return statement below!
-		return (
-			<div className="App">
-				<CatComponent />
-				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
-			</div>
-		);
-	}
-}
-
-export default App;
+;
